36d33185cf5880116704fcc420825f3e005be124
Mario Sieg (Neo) <support@kerbogames.com>
2024-12-06 15:52:57 +0100

    Removed build folder

--- build/release/third_party/libuv/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mario/Documents/remote_projects/pccl-refactor/third_party/libuv
-# Build directory: /home/mario/Documents/remote_projects/pccl-refactor/build/release/third_party/libuv
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
