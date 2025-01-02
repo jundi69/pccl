0edaee242e49c9d5d0f43c63a51219dc5916686e
mike <michael.keiblinger@gmail.com>
2025-01-03 00:03:39 +0100

    Re-enable unit tests

--- python/tests/unit_tests/pccl_test.py
+++ python/tests/unit_tests/pccl_test.py
@@ -8,13 +8,11 @@ def test_import_lib():
 from pccl import *
 
 def test_master_node_run():
-    return
     m = MasterNode(listen_address='127.0.0.1:48148')
     m.run()
     m.interrupt()
 
 def test_communicator():
-    return
     m = MasterNode(listen_address='127.0.0.1:48148')
     m.run()
     c = Communicator('127.0.0.1:48148', 0)
