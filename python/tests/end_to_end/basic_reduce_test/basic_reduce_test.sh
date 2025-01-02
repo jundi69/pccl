7cd13aa9575d88a4915e0cfff2b130353f85b414
mike <michael.keiblinger@gmail.com>
2025-01-02 21:28:04 +0100

    use bash for launching e2e tests

---
+++ python/tests/end_to_end/basic_reduce_test/basic_reduce_test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export PCCL_LOG_LEVEL=DEBUG
+
+python master.py &
+MASTER_PID=$!
+echo "Started master.py with PID $MASTER_PID"
+
+sleep 5
+
+python peer.py &
+PEER1_PID=$!
+echo "Started first peer.py with PID $PEER1_PID"
+
+python peer.py &
+PEER2_PID=$!
+echo "Started second peer.py with PID $PEER2_PID"
+
+cleanup() {
+    echo "Terminating master.py with PID $MASTER_PID"
+    kill $MASTER_PID
+    exit
+}
+
+trap cleanup SIGINT SIGTERM
+
+wait $PEER1_PID
+echo "First peer.py (PID $PEER1_PID) has exited."
+
+wait $PEER2_PID
+echo "Second peer.py (PID $PEER2_PID) has exited."
+
+echo "Both peers have exited. Terminating master.py."
+kill $MASTER_PID
+
+wait $MASTER_PID
+echo "master.py has been terminated."
