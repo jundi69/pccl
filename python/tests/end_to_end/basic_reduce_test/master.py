72bb72eb3ee8c5eca57f6361da2d0877ae8352cd
mike <michael.keiblinger@gmail.com>
2025-01-02 23:45:38 +0100

    one last experiment

--- python/tests/end_to_end/basic_reduce_test/master.py
+++ python/tests/end_to_end/basic_reduce_test/master.py
@@ -7,10 +7,10 @@ HOST: str = '0.0.0.0:48148'
 
 
 def main():
-    print(f"Starting master node on {HOST}")
-    master: MasterNode = MasterNode(listen_address=HOST)
-    master.run()
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    print(f"Starting master node on {HOST}")
+    master: MasterNode = MasterNode(listen_address=HOST)
+    master.run()
