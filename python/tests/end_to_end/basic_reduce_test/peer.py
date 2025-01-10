b807f20f33a378d46bea3c1c0b48117b1790ed0f
Jackmin801 <ongjackm@gmail.com>
2025-01-10 00:38:02 +0000

    refactor: simplify exiting logic in peer test

--- python/tests/end_to_end/basic_reduce_test/peer.py
+++ python/tests/end_to_end/basic_reduce_test/peer.py
@@ -1,15 +1,17 @@
 from time import sleep
 import os
 import logging
-from pccl import *
+import torch
+from pccl import SharedState, TensorInfo, Communicator, Attribute, ReduceOp
 
 HOST: str = '127.0.0.1:48148'
-STEPS: int = 10000
+STEPS: int = 1000
 WEIGHT_N: int = 1024
 PEERS: int = 1
 NUM_ELEMENTS: int = 1024
 RANK: int = int(os.getenv('RANK', "0"))
 
+logging.basicConfig(level=logging.INFO)
 
 def main():
     logging.info(f"(RANK={RANK}) Starting peer node connecting to {HOST}")
@@ -29,10 +31,9 @@ def main():
     world_size: int = communicator.get_attribute(Attribute.CURRENT_WORLD_SIZE)
 
     n_performed_steps = 0
-    i = 0
     while n_performed_steps < STEPS:
-        if i > 0 or world_size == 1:
-            logging.info(f"(RANK={RANK}, it={i}) update_topology()")
+        if world_size == 1:
+            logging.info(f"(RANK={RANK}, it={n_performed_steps}) update_topology()")
             communicator.update_topology()
         world_size = communicator.get_attribute(Attribute.CURRENT_WORLD_SIZE)
 
@@ -40,25 +41,22 @@ def main():
             sleep(1)
             continue
 
-        logging.info(f"(RANK={RANK}, it={i}) sync_shared_state()")
+        logging.info(f"(RANK={RANK}, it={n_performed_steps}) sync_shared_state()")
         communicator.sync_shared_state(shared_state)
-
-        if shared_state.revision == STEPS:
-            break
 
         # Create gradients tensors
         grad: torch.Tensor = torch.rand(NUM_ELEMENTS, dtype=torch.float32)
         while True:
-            logging.info(f"(RANK={RANK}, it={i}) all_reduce_async()")
+            logging.info(f"(RANK={RANK}, it={n_performed_steps}) all_reduce_async()")
             handle = communicator.all_reduce_async(grad, weights, numel=NUM_ELEMENTS, op=ReduceOp.SUM)
             is_success, status, info = handle.wait()
-            assert is_success == True, f"All reduce failed with stats: {status}"
+            assert is_success, f"All reduce failed with stats: {status}"
             assert info is not None
-            logging.info(f"((RANK={RANK}, it={i}) Reduce completed RX: {info.rx_bytes}, TX: {info.tx_bytes}")
+            logging.info(f"((RANK={RANK}, it={n_performed_steps}) Reduce completed RX: {info.rx_bytes}, TX: {info.tx_bytes}")
             break
 
         shared_state.revision += 1
-        i += 1
+        n_performed_steps += 1
 
     logging.info(f"(RANK={RANK}) Finished")
 
@@ -66,6 +64,6 @@ def main():
 if __name__ == '__main__':
     try:
         main()
-    except (Exception, AssertionError) as e:
+    except Exception as e:
         print(f"(RANK={RANK}) failed with: {e}")
         raise e
