59e79e1156d1634b320cee241b4a4b780930e681
mike <michael.keiblinger@gmail.com>
2025-02-17 07:39:47 +0000

    add multi-device-type train e2e test

--- python/tests/end_to_end/mnist_ddp/mnist_ddp_e2e_test.py
+++ python/tests/end_to_end/mnist_ddp/mnist_ddp_e2e_test.py
@@ -115,6 +115,31 @@ def test_mnist_ddp_world_size_2_plus_1_late_joiner(use_cuda: bool):
     master_process.kill()
     master_process.wait()
 
+@unittest.skip("Enable test when distance-aware hashes are implemented")
+@pytest.mark.skipif(not pccl.cuda.is_available(), reason="PCCL compiled without cuda support, skipping test")
+def test_mnist_train_peers_different_devices():
+    peer_script_path = os.path.join(os.path.dirname(__file__), 'mnist_peer.py')
+    master_script_path = os.path.join(os.path.dirname(__file__), 'mnist_master.py')
+
+    # launch master node
+    master_process = launch_py_process(master_script_path, [], {'PCCL_LOG_LEVEL': 'INFO'})
+    print(f"Launched master node; PID: {master_process.pid}")
+
+    # launch 2 peers
+    process_list = []
+    for rank in range(2):
+        process_list.append(launch_py_process(peer_script_path, [], {'PCCL_LOG_LEVEL': 'INFO', 'RANK': str(rank), 'MNIST_USE_CUDA': "1" if rank == 0 else "0"}))
+        print(f"Launched peer {rank}; PID: {process_list[-1].pid}")
+
+    # wait for all processes to finish
+    for process in process_list:
+        exit_code = process.wait()
+        assert exit_code == 0, "Peer process exited with non-zero exit code"
+
+    # kill master process
+    master_process.kill()
+    master_process.wait()
+
 
 @pytest.mark.parametrize("use_cuda", [False, True] if pccl.cuda.is_available() else [False])
 @unittest.skip("Skipping test_mnist_ddp_world_size_4")
